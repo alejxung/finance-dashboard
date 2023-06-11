abae8124994ae5507943e1eaff8c95d77477f46c
Λlex Jung <jjhyuk02@gmail.com>
2023-06-11 14:11:44 +0000

    MongoDB Installation and Setup

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 
 /* CONFIGURATIONS */
-dotenv.config;
+dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
@@ -16,3 +16,16 @@ app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
+
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 9000;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(async () => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
+  
